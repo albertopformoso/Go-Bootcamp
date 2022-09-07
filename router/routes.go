c415ff276e9e94dde347c3437ca4645a7a8dd416
albertopformoso <albertopformoso@gmail.com>
2022-09-06 21:24:29 -0500

    provide pokemons endpoint added

---
+++ router/routes.go
@@ -0,0 +1,11 @@
+package router
+
+import (
+	"net/http"
+
+	"github.com/albertopformoso/Go-Bootcamp/controller"
+)
+
+func Routes(router *http.ServeMux, ctrl controller.API) {
+	router.HandleFunc("/api/v1/provide", ctrl.FillCSV)
+}
