da4ecd57dc43d49da77e2f9226fbe199a9c1ac2a
albertopformoso <albertopformoso@gmail.com>
2022-09-07 09:56:02 -0500

    get pokemons added

---
+++ repository/api.go
@@ -0,0 +1,49 @@
+package repository
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+
+	"github.com/albertopformoso/Go-Bootcamp/model"
+)
+
+type PokeAPI struct{}
+
+const url = "https://pokeapi.co/api/v2/"
+
+func (pa PokeAPI) FetchPokemon(id int) (model.Pokemon, error) {
+	pokemon := model.Pokemon{}
+
+	pokeUrl := fmt.Sprintf("%s/pokemon/%d", url, id)
+	resp, err := http.Get(pokeUrl)
+	if err != nil {
+		log.Println("ERROR:", err)
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Println("ERROR:", err)
+	}
+
+	// Unmarshal result
+	err = json.Unmarshal(body, &pokemon)
+	if err != nil {
+		log.Printf("Reading body failed: %s", err)
+	}
+
+	return pokemon, nil
+}
+
+func (pa PokeAPI) GetPokemons() ([]model.Pokemon, error) {
+    ls := LocalStorage{}
+    pokemons, err := ls.Read()
+    if err != nil {
+        return nil, err
+    }
+
+    return pokemons, nil
+}
